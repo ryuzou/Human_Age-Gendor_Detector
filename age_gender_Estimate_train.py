77d0635c5477df6a35a7d6fd3e78d4a6f90903a7
ryuzou <ryuzot.1112@gmail.com>
2018-09-10 19:20:52 +0900

    commit for clouding, NOT IN USE YET

--- age_gender_Estimate_train.py
+++ age_gender_Estimate_train.py
@@ -1,75 +1,70 @@
+import os.path,sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__))+'/../../')
+#os.environ['KERAS_BACKEND'] = 'theano'
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Input
+from keras.layers import Dense, GlobalAveragePooling2D,Input
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD
-from keras.callbacks import CSVLogger
+import keras.callbacks
+import scipy.ndimage
 
-n_categories = 20
-batch_size = 32
-train_dir = 'Dataset/traindat'
-validation_dir = 'Dataset/testdat'
-file_name = 'vgg16_facegender_fine'
+N_CATEGORIES  = 20
+IMAGE_SIZE = 224
+BATCH_SIZE = 16
 
-base_model = VGG16(weights='imagenet', include_top=False,
-                   input_tensor=Input(shape=(224, 224, 3)))
+NUM_TRAINING = sum([len(files) for r, d, files in os.walk("Dataset/traindat/")])
+NUM_VALIDATION = sum([len(files) for r, d, files in os.walk("Dataset/testdat/")])
+#NUM_TRAINING = 30000
 
-# add new layers instead of FC networks
+input_tensor = Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+base_model = VGG16(weights='imagenet', include_top=False,input_tensor=input_tensor)
+
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
-prediction = Dense(n_categories, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=prediction)
+predictions = Dense(N_CATEGORIES, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
 
-# fix weights before VGG16 14layers
-for layer in base_model.layers[:15]:
-    layer.trainable = False
-
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+for layer in base_model.layers:
+   layer.trainable = False
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy',metrics=['accuracy'])
 
 model.summary()
-base_model = VGG16(weights='imagenet', include_top=False,
-                   input_tensor=Input(shape=(224, 224, 3)))
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-prediction = Dense(n_categories, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=prediction)
-
-for layer in base_model.layers[:15]:
-    layer.trainable = False
 
 train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+   rescale=1.0 / 255,
+   shear_range=0.2,
+   zoom_range=0.2,
+   horizontal_flip=True,
+   rotation_range=10)
 
-validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
-
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=(224, 224),
-    batch_size=batch_size,
-    class_mode='categorical',
-    shuffle=True
+test_datagen = ImageDataGenerator(
+   rescale=1.0 / 255,
 )
 
-validation_generator = validation_datagen.flow_from_directory(
-    validation_dir,
-    target_size=(224, 224),
-    batch_size=batch_size,
-    class_mode='categorical',
-    shuffle=True
+train_generator = train_datagen.flow_from_directory(
+   'Dataset/traindat',
+   target_size=(IMAGE_SIZE, IMAGE_SIZE),
+   batch_size=BATCH_SIZE,
+   class_mode='categorical',
+   shuffle=True
+)
+
+validation_generator = test_datagen.flow_from_directory(
+   'Dataset/traindat',
+   target_size=(IMAGE_SIZE, IMAGE_SIZE),
+   batch_size=BATCH_SIZE,
+   class_mode='categorical',
+   shuffle=True
 )
 
 hist = model.fit_generator(train_generator,
-                           epochs=200,
-                           verbose=1,
-                           validation_data=validation_generator,
-                           callbacks=[CSVLogger(file_name + '.csv')])
+   steps_per_epoch=NUM_TRAINING//BATCH_SIZE,
+   epochs=50,
+   verbose=1,
+   validation_data=validation_generator,
+   validation_steps=NUM_VALIDATION//BATCH_SIZE,
+   )
 
-# save weights
-model.save(file_name + '.h5')
+model.save('face_gender_age.hdf5')
